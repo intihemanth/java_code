78b067c68c9a54c321825c17fb17336ac3cfe8ee
intihemanth <intihemanth01@gmail.com>
2022-12-14 10:45:42 +0530

    java app

---
+++ src/main/java/Selinium/Practice/App.java
@@ -0,0 +1,13 @@
+package Selinium.Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
